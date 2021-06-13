987eca25c88b9993717c946dc9f098a36fe9847d
PYlamp <73482519+PYlamp@users.noreply.github.com>
2021-06-12 20:52:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from web3 import web3
+from web3 import Web3
 
 ganache_url = "http://127.0.0.1:7545"
 web3 = web3(web3.HttpProvider(ganache_url))
 
-print(web3.isConnected)
+print(web3.isConnected())
 
