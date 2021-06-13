4776db48f7fa9ba759432900413e184c8181d9ce
PYlamp <73482519+PYlamp@users.noreply.github.com>
2021-06-12 21:52:47 -0500

    Update app.py
    another update, another failed attempt at getting this import to work

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 
 ganache_url = "http://127.0.0.1:7545"
-web3 = web3(web3.HttpProvider(ganache_url))
+web3 = Web3(Web3.HttpProvider(ganache_url))
 
 print(web3.isConnected())
 
